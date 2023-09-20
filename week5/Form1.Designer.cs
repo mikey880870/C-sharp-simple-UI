9b3f71d4224723027d656a0eef2f8feed2d5a95c
Hsieh Po Yen <127172104+mikey880870@users.noreply.github.com>
2023-09-20 15:02:07 +0800

    C# code

---
+++ week5/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace week5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StartBTN = new System.Windows.Forms.Button();
+            this.StopBTN = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EnlargeTXB = new System.Windows.Forms.TextBox();
+            this.checkbox = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ExitBTN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "導入圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StartBTN
+            // 
+            this.StartBTN.Location = new System.Drawing.Point(85, 140);
+            this.StartBTN.Name = "StartBTN";
+            this.StartBTN.Size = new System.Drawing.Size(160, 48);
+            this.StartBTN.TabIndex = 1;
+            this.StartBTN.TabStop = false;
+            this.StartBTN.Text = "開始";
+            this.StartBTN.UseVisualStyleBackColor = true;
+            this.StartBTN.Click += new System.EventHandler(this.StartBTN_Click);
+            // 
+            // StopBTN
+            // 
+            this.StopBTN.Location = new System.Drawing.Point(85, 217);
+            this.StopBTN.Name = "StopBTN";
+            this.StopBTN.Size = new System.Drawing.Size(160, 48);
+            this.StopBTN.TabIndex = 2;
+            this.StopBTN.Text = "暫停";
+            this.StopBTN.UseVisualStyleBackColor = true;
+            this.StopBTN.Click += new System.EventHandler(this.StopBTN_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(271, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(422, 371);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(714, 22);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(603, 465);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(26, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "放大倍率：";
+            // 
+            // EnlargeTXB
+            // 
+            this.EnlargeTXB.Location = new System.Drawing.Point(132, 372);
+            this.EnlargeTXB.Name = "EnlargeTXB";
+            this.EnlargeTXB.Size = new System.Drawing.Size(113, 25);
+            this.EnlargeTXB.TabIndex = 6;
+            // 
+            // checkbox
+            // 
+            this.checkbox.AutoSize = true;
+            this.checkbox.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkbox.Location = new System.Drawing.Point(30, 416);
+            this.checkbox.Name = "checkbox";
+            this.checkbox.Size = new System.Drawing.Size(111, 24);
+            this.checkbox.TabIndex = 7;
+            this.checkbox.Text = "扣除基線";
+            this.checkbox.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ExitBTN
+            // 
+            this.ExitBTN.Location = new System.Drawing.Point(85, 300);
+            this.ExitBTN.Name = "ExitBTN";
+            this.ExitBTN.Size = new System.Drawing.Size(160, 48);
+            this.ExitBTN.TabIndex = 8;
+            this.ExitBTN.Text = "結束";
+            this.ExitBTN.UseVisualStyleBackColor = true;
+            this.ExitBTN.Click += new System.EventHandler(this.ExitBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1380, 624);
+            this.Controls.Add(this.ExitBTN);
+            this.Controls.Add(this.checkbox);
+            this.Controls.Add(this.EnlargeTXB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.StopBTN);
+            this.Controls.Add(this.StartBTN);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button StartBTN;
+        private System.Windows.Forms.Button StopBTN;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EnlargeTXB;
+        private System.Windows.Forms.CheckBox checkbox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button ExitBTN;
+    }
+}
+
---
+++ week5/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace week5
+{
+    public partial class Form1 : Form
+    {
+        public int itask=0;
+        public List<double> data = new List<double>();
+        public List<double> EnlargeData = new List<double>();
+        public List<double> BaseData = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            openfile();                 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            openfile();
+        }
+
+        public void openfile()             //開啟檔案並show在picturebox
+        {
+            OpenFileDialog newfile = new OpenFileDialog();
+            newfile.Filter = "All Files|*.*";
+            if (newfile.ShowDialog() == DialogResult.OK)
+            {
+                string filepath = newfile.FileName;
+                pictureBox1.Image = Image.FromFile(filepath);
+            }
+        }
+
+        private void StartBTN_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            itask = 1;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            process();
+        }
+       
+        private bool process()
+        {
+            switch (itask)
+            {
+                case 0:
+                    break;
+                case 1:
+                    itask = 2;
+                    Transfer_To_Data();            //將圖片轉數據並畫出
+                    break;
+                case 2:
+                    timer1.Stop();
+                    if (chart1.Series[0].AxisLabel != null) {
+                        MessageBox.Show("1請輸入放大倍率!!");
+                    }
+                    StartBTN.Enabled = false;
+                    StopBTN.Text = "繼續";
+                    break;
+                case 3:
+                    itask = 4;
+                    enlarge();                    //將數據放大並畫出
+                    break;
+                case 4:
+                    timer1.Stop();
+                    MessageBox.Show("是否扣除基線");
+                    StopBTN.Text = "繼續";
+                    break;
+                case 5:
+                    itask = 0;
+                    StopBTN.Text = "暫停";
+                    break;
+            }
+            return true; 
+        }
+
+        private void StopBTN_Click(object sender, EventArgs e)
+        {
+            if(itask == 2)
+            {
+                if (EnlargeTXB.Text == "")
+                    MessageBox.Show("請輸入放大倍率!!");
+                else
+                {
+                    itask = 3;
+                    timer1.Start();
+                    StopBTN.Text = "暫停";
+                }
+                    
+            }
+            
+            if (itask == 4)
+            {
+                double basedata=0, totaldata=0;
+                if (checkbox.Checked)
+                {
+                   for(int i = 0; i < 51; i++)          //計算基線值
+                    {
+                        totaldata += EnlargeData[i];
+                    }
+                    basedata = totaldata / 50;
+                    for(int j=0; j < EnlargeData.Count; j++)
+                    {
+                        BaseData.Add(EnlargeData[j] - basedata);
+                    }
+                    chart1.Series[0].Points.Clear();
+                    chart1.Series[0].Points.AddXY(0, 0);
+                    for (int i = 0; i < BaseData.Count; i++)
+                    {
+                        chart1.Series[0].Points.AddXY(i, BaseData[i]);
+                    }
+                    itask = 5;
+                    timer1.Start();
+                }
+            }
+        }
+        public void Transfer_To_Data()                       //將圖片轉灰階並畫出波形
+        {
+            int gray;
+            double total = 0;
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                total = 0;
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    gray = (
+                    bitmap.GetPixel(x, y).R +
+                    bitmap.GetPixel(x, y).G +
+                    bitmap.GetPixel(x, y).B) / 3;
+                    Color color = Color.FromArgb(gray, gray, gray);
+                    bitmap.SetPixel(x, y, color);
+                    total += gray;
+                }
+                data.Add(total / 960);
+            }
+            chart1.ChartAreas[0].AxisY.Maximum = 260;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            for (int i = 0; i < data.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, data[i]);
+            }
+        }
+        public void enlarge()     //將數據放大並畫出
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                EnlargeData.Add(data[i] * double.Parse(EnlargeTXB.Text));
+            }
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, 0);
+            for (int i = 0; i < EnlargeData.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, EnlargeData[i]);
+            }
+        }
+
+        private void ExitBTN_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
